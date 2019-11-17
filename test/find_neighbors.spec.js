dd23d9a69b535ec15110cd42dde6f02265b8783d
Gabriel Birke <gabriel.birke@gmail.com>
2019-11-17 22:55:06 +0100

    Improve unit test
    Add linebreaks that resemble the grid

--- test/find_neighbors.spec.js
+++ test/find_neighbors.spec.js
@@ -19,43 +19,69 @@ describe('findNeighborsConditional', () => {
 		});
 
 		it('should return all neighbors from center of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 1, 1 ), [DEAD, ALIVE, DEAD, DEAD, DEAD, DEAD, ALIVE, DEAD, ] );
-		});
-
-		it('should return all neighbors from center of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 1, 1 ), [DEAD, ALIVE, DEAD, DEAD, DEAD, DEAD, ALIVE, DEAD, ] );
+			assert.deepEqual( findNeighborsConditional( grid, 1, 1 ), [
+				DEAD, ALIVE, DEAD, 
+				DEAD, DEAD, 
+				DEAD, ALIVE, DEAD, 
+			] );
 		});
 
 		it('should return all neighbors from top left of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 0, 0 ), [ALIVE, DEAD, ALIVE, ] );
+			assert.deepEqual( findNeighborsConditional( grid, 0, 0 ), [
+				ALIVE, 
+				DEAD, ALIVE, 
+			] );
 		});
 
 		it('should return all neighbors from top of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 0, 1 ), [DEAD, DEAD, DEAD, ALIVE, DEAD, ] );
+			assert.deepEqual( findNeighborsConditional( grid, 0, 1 ), [
+				DEAD, DEAD, 
+				DEAD, ALIVE, DEAD, 
+			] );
 		});
 
 		it('should return all neighbors from top right of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 0, 2 ), [ ALIVE, ALIVE, DEAD ] );
+			assert.deepEqual( findNeighborsConditional( grid, 0, 2 ), [ 
+				ALIVE, 
+				ALIVE, DEAD 
+			] );
 		});
 
 		it('should return all neighbors from left side of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 1, 0 ), [ DEAD, ALIVE, ALIVE, DEAD, ALIVE ] );
+			assert.deepEqual( findNeighborsConditional( grid, 1, 0 ), [ 
+				DEAD, ALIVE, 
+				ALIVE, 
+				DEAD, ALIVE 
+			] );
 		});
 
 		it('should return all neighbors from right side of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 1, 2 ), [ ALIVE, DEAD, ALIVE, ALIVE, DEAD ] );
+			assert.deepEqual( findNeighborsConditional( grid, 1, 2 ), [ 
+				ALIVE, DEAD, 
+				ALIVE, 
+				ALIVE, DEAD 
+			] );
 		});
 
 		it('should return all neighbors from bottom left of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 2, 0 ), [ DEAD, ALIVE, ALIVE ] );
+			assert.deepEqual( findNeighborsConditional( grid, 2, 0 ), [ 
+				DEAD, ALIVE, 
+				ALIVE 
+			] );
 		});
 
 		it('should return all neighbors from bottom of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 2, 1 ), [ DEAD, ALIVE, DEAD, DEAD, DEAD ] );
+			assert.deepEqual( findNeighborsConditional( grid, 2, 1 ), [ 
+				DEAD, ALIVE, DEAD, 
+				DEAD, DEAD 
+			] );
 		});
 
 		it('should return all neighbors from bottom right of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsConditional( grid, 2, 2 ), [ ALIVE, DEAD, ALIVE ] );
+			assert.deepEqual( findNeighborsConditional( grid, 2, 2 ), [ 
+				ALIVE, DEAD, 
+				ALIVE 
+			] );
 		});
 	});
 
@@ -67,11 +93,11 @@ describe('findNeighborsConditional', () => {
 		});
 
 		it('should return all neighbors from center of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsFunctional( grid, 1, 1 ), [DEAD, ALIVE, DEAD, DEAD, DEAD, DEAD, ALIVE, DEAD, ] );
-		});
-
-		it('should return all neighbors from center of 3x3 grid', () => {
-			assert.deepEqual( findNeighborsFunctional( grid, 1, 1 ), [DEAD, ALIVE, DEAD, DEAD, DEAD, DEAD, ALIVE, DEAD, ] );
+			assert.deepEqual( findNeighborsFunctional( grid, 1, 1 ), [
+				DEAD, ALIVE, DEAD, 
+				DEAD, DEAD, 
+				DEAD, ALIVE, DEAD, 
+			] );
 		});
 
 		it('should return all neighbors from top left of 3x3 grid', () => {
