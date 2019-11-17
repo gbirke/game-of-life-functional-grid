3453e7d937018c5d629d69b69bd168f02b4bd1bb
Gabriel Birke <gabriel.birke@gmail.com>
2019-11-17 16:25:21 +0100

    Remove index files and improve package info
    Remove index.js - This is an example code package that doesn't need it
    Un-shorten keywords and fix typo in description

--- src/index.js
+++
@@ -1,3 +0,0 @@
-export const sayHello = _ => "Hello people!"
-
-console.log(sayHello())
--- test/index.spec.js
+++
@@ -1,8 +0,0 @@
-import {strict as assert} from 'assert';
-import { sayHello } from '../src/index.js';
-
-describe('Test environment', () => {
-	it('should work', () => {
-		assert.equal( sayHello(), 'Hello people!');
-	});
-});
