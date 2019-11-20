c3f5b40806f51d359d9ce69a06b5fc78961c974d
Gabriel Birke <gabriel.birke@gmail.com>
2019-11-20 22:24:48 +0100

    Optimize functional solution
    "Pre-render" the coordinates.

--- src/find_neighbors_functional_optimized.js
+++ src/find_neighbors_functional_optimized.js
@@ -1,16 +1,14 @@
-function generateCoordinates( coordinate ) {
-	return [ -1, 0, 1 ].map( modifier => coordinate + modifier );
-}
-
 export function findNeighbors( grid, row, col ) {
 	const maxRow = grid.length;
 	const maxCol = grid[0].length;
-	const validCoordinate = ( [ r, c ] ) => r >= 0 && r < maxRow && c >= 0 && c < maxCol && !(r === row && c === col);
 
-	// Cartesian product of generated row and col coordinates,
-	// Inspired by https://stackoverflow.com/q/12303989/130121
-	const coordinates = generateCoordinates( row ).flatMap( r => generateCoordinates( col ).map( c => [ r, c ] ) )
+	const coordinates = [
+		[row-1, col-1], [row-1, col], [row-1, col+1],
+		[row,   col-1],               [row,   col+1],
+		[row+1, col-1], [row+1, col], [row+1, col+1],
+	]
 	
+	const validCoordinate = ( [ r, c ] ) => r >= 0 && r < maxRow && c >= 0 && c < maxCol;
 	return coordinates.reduce( ( cells, coordinate ) => {
 		if ( validCoordinate( coordinate ) ) {
 			cells.push( grid[ coordinate[0] ][ coordinate[1] ] );
